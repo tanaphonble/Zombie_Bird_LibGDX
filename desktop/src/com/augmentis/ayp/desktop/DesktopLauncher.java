6408393c1b323267a25131fa3fa71c0ed0b78e52
Tanaphon <tanaphon.a@augmentis.biz>
2016-08-30 19:50:50 +0700

    initialize libgdb project

---
+++ desktop/src/com/augmentis/ayp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.augmentis.ayp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.augmentis.ayp.ZBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZBGame(), config);
+	}
+}
---
+++ html/src/com/augmentis/ayp/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.augmentis.ayp.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.augmentis.ayp.ZBGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ZBGame();
+        }
+}
