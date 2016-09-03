acb02c59b08e6479b6bc1683ac80758a1cbc0263
Tanaphon <tanaphon.a@augmentis.biz>
2016-09-03 17:32:45 +0700

    remove read rectangle(bound of scrollable objects)

--- core/src/com/augmentis/ayp/gemeworld/GameRenderer.java
+++ core/src/com/augmentis/ayp/gemeworld/GameRenderer.java
@@ -157,8 +157,8 @@ public class GameRenderer {
 
         // 1. Draw grass
         drawGrass();
-
         batcher.enableBlending();
+
         // 2. Draw Pipes
         drawPipes();
 
@@ -192,49 +192,5 @@ public class GameRenderer {
         }
 
         batcher.end();
-
-        shapeRenderer.begin(ShapeType.Filled);
-        shapeRenderer.setColor(Color.RED);
-        shapeRenderer.circle(bird.getBoundingCircle().x, bird.getBoundingCircle().y,
-                bird.getBoundingCircle().radius);
-
-        /*
-         * Excuse the mass below. Temporary code for testing bounding rectangles.
-         */
-        // Bar up for pipes 1 2 and 3
-        shapeRenderer.rect(pipe1.getBarUp().x, pipe1.getBarUp().y, pipe1.getBarUp().width,
-                pipe1.getBarUp().height);
-        shapeRenderer.rect(pipe2.getBarUp().x, pipe2.getBarUp().y, pipe2.getBarUp().width,
-                pipe2.getBarUp().height);
-        shapeRenderer.rect(pipe3.getBarUp().x, pipe3.getBarUp().y, pipe3.getBarUp().width,
-                pipe3.getBarUp().height);
-
-        // Bar down for pipes 1 2 and 3
-        shapeRenderer.rect(pipe1.getBarDown().x, pipe1.getBarDown().y, pipe1.getBarDown().width,
-                pipe1.getBarDown().height);
-        shapeRenderer.rect(pipe2.getBarDown().x, pipe2.getBarDown().y, pipe2.getBarDown().width,
-                pipe2.getBarDown().height);
-        shapeRenderer.rect(pipe3.getBarDown().x, pipe3.getBarDown().y, pipe3.getBarDown().width,
-                pipe3.getBarDown().height);
-
-        // Skull up for pipes 1 2 and 3
-        shapeRenderer.rect(pipe1.getSkullUp().x, pipe1.getSkullUp().y, pipe1.getSkullUp().width,
-                pipe1.getSkullUp().height);
-        shapeRenderer.rect(pipe2.getSkullUp().x, pipe2.getSkullUp().y, pipe2.getSkullUp().width,
-                pipe2.getSkullUp().height);
-        shapeRenderer.rect(pipe3.getSkullUp().x, pipe3.getSkullUp().y, pipe3.getSkullUp().width,
-                pipe3.getSkullUp().height);
-
-        // Skull down for pipes 1 2 and 3
-        shapeRenderer.rect(pipe1.getSkullDown().x, pipe1.getSkullDown().y, pipe1.getSkullDown().width,
-                pipe1.getSkullDown().height);
-        shapeRenderer.rect(pipe2.getSkullDown().x, pipe2.getSkullDown().y, pipe2.getSkullDown().width,
-                pipe2.getSkullDown().height);
-        shapeRenderer.rect(pipe3.getSkullDown().x, pipe3.getSkullDown().y, pipe3.getSkullDown().width,
-                pipe3.getSkullDown().height);
-
-        shapeRenderer.end();
     }
-
-
 }
